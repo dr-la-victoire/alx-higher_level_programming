794ed960627dd351964de6417fbf73536cbdb83e
la victoire <lavictoire633@gmail.com>
2023-06-05 05:57:55 -0700

    string manipulation

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+last = word[7:9]
+middle = word[1:-1]
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {last}")
+print(f"Middle word: {middle}")

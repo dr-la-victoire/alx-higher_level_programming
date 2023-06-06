159c2508c30cbaa34ff80d46e6eb9bd3497f1f3f
la victoire <lavictoire633@gmail.com>
2023-06-06 12:42:40 -0700

    printing all possible combinations of digits in ascending order and stuff

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(1, 10):
         if i != j and i < j:
-            if i != 8 and j != 9:
+            if i != 8:
                 print("{}{}, ".format(i, j), end="")
-            elif i == 8 and j == 9:
+            else:
                 print("{}{}".format(i, j))

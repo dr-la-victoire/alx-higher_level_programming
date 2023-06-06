a5195f75ae3a9da53539d58d434dc110553a98ac
la victoire <lavictoire633@gmail.com>
2023-06-06 07:10:17 -0700

    making correction (pycodestyle)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 # printing the last digit
-new_number = abs(number) % 10
+new = abs(number) % 10
 # checking if the number is -ve
 if number < 0:
-    new_number = -new_number
+    new = -new
 if new_number > 5:
-    print(f'Last digit of {number} is {new_number} and is greater than 5')
+    print(f'Last digit of {number} is {new} and is greater than 5')
 elif new_number < 6 and new_number != 0:
-    print(f'Last digit of {number} is {new_number} and is less than 6 and not 0')
+    print(f'Last digit of {number} is {new} and is less than 6 and not 0')
 else:
-    print(f'Last digit of {number} is {new_number} and is 0')
+    print(f'Last digit of {number} is {new} and is 0')

8fcb0e66e0b457402eb54bd11d6b7ad379bde813
la victoire <lavictoire633@gmail.com>
2023-06-06 07:12:40 -0700

    making correction (pycodestyle)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ new = abs(number) % 10
 # checking if the number is -ve
 if number < 0:
     new = -new
-if new_number > 5:
+if new > 5:
     print(f'Last digit of {number} is {new} and is greater than 5')
-elif new_number < 6 and new_number != 0:
+elif new < 6 and new != 0:
     print(f'Last digit of {number} is {new} and is less than 6 and not 0')
 else:
     print(f'Last digit of {number} is {new} and is 0')

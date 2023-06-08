b3153880603bb9616b4a1ea1bbf7f6ff826b6297
la victoire <lavictoire633@gmail.com>
2023-06-08 11:48:02 -0700

    I think i am having errors because i failed to add the main guard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 '''This program prints arguments passed as CL'''
 # Importing the module that does this task
 from sys import argv
-# Condition to check the number of arguments
-if len(argv) - 1 == 0:
-    print("{} arguments.".format(len(argv) - 1))
-elif len(argv) - 1 == 1:
-    print("{} argument:".format(len(argv) - 1))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-# Loop to print the arguments
-if len(argv) - 1 != 0:
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    # Condition to check the number of arguments
+    if len(argv) - 1 == 0:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+    # Loop to print the arguments
+    if len(argv) - 1 != 0:
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))

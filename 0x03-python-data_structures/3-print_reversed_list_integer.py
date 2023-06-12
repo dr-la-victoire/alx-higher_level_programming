0ebb206aedb14b1ffcfb554fd705662120253677
la victoire <lavictoire633@gmail.com>
2023-06-12 12:59:39 -0700

    fixing errors: i added the format specifier for integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     '''This function prints all the integers in a list in reverse order'''
     for reverse in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[reverse]))
+        print("{:d}".format(my_list[reverse]))

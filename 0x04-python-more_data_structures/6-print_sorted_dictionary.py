f84446ad24d27c7d920d530e09379cc68a353416
la victoire <lavictoire633@gmail.com>
2023-06-14 13:49:18 -0700

    a function that prints a dictionary in sorted order

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     '''This function prints a dictionary in sorted order'''
-    sorted_dictionary = dict(sorted(a_dictionary.items()))
-    print(sorted_dictionary)
+    # turning the dictionary to a list of dictionary keys
+    dict_list = list(a_dictionary.keys())
+    # sorting the list of keys
+    dict_list.sort()
+    # printing the sorted list
+    for keys in dict_list:
+        print("{} : {}".format(keys, a_dictionary[keys]))

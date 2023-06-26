ef17c2ce9a373f9472b4cbd51d896e8269bd3c35
la victoire <lavictoire633@gmail.com>
2023-06-26 13:31:48 -0700

    fixing slight errors: i assigned 0.0 instead of 0 to the elements that had errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,13 +8,13 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except TypeError:  # if the current index isn't an int or float
             print("wrong type")
-            result = 0.0
+            result = 0
         except ZeroDivisionError:  # if my_list_2[i] == 0
             print("division by 0")
-            result = 0.0
+            result = 0
         except IndexError:  # if one of the list is smaller than the other one
             print("out of range")
-            result = 0.0
+            result = 0
         finally:
             list_3.append(result)
     return (list_3)

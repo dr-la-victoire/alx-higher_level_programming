9d475993b11bdaa55fc2ad5f933c25e5873b43ca
la victoire <lavictoire633@gmail.com>
2023-06-26 08:15:02 -0700

    fixing errors: i changed my return statement in the while loop to a break statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
             i += 1
             num += 1
         except IndexError:
-            return (num)
+            break
     print("")
     return (num)

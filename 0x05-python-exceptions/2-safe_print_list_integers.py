23fd13110eec3f1375e98b4e5bc4a1f67e82f115
la victoire <lavictoire633@gmail.com>
2023-06-26 09:47:19 -0700

    finally figured out what was wrong with this code: the return dtatement was indented inside the for loop, hence the function existed prematurely

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,5 +11,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass # it means no specific action will be taken
 
-        print("")
-        return (num)
+    print("")
+    return (num)

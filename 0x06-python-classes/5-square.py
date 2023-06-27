2d3406bd5bbc4e950dc6767b3705f4465ca48342
la victoire <lavictoire633@gmail.com>
2023-06-27 12:57:47 -0700

    a class that defines a square and prints it to stdoout

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This class defines a square with the size attribute
+
+    The size attribute is a private instance attribute
+    It has a default value of 0 if nothing has been provided
+"""
+
+
+class Square:
+    """Defines a class with an attribute size"""
+    def __init__(self, size=0):
+        """The initialization of the class"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self._size = size
+
+    def area(self):
+        """This method calculates the area of a square"""
+        return (self._size * self._size)
+
+    @property
+    def size(self):
+        """This method is the getter method to retrieve the size"""
+        return (self._size)
+
+    @size.setter
+    def size(self, value):
+        """This method is the setter method to set the size to a value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = value
+
+    def my_print(self):
+        """This method prints the square with char #"""
+        if self._size < 0:
+            raise ValueError("size must be >= 0")
+        elif self._size > 0:
+            for i in range(self._size):
+                for j in range(self._size):
+                    print("#", end="")
+                print()
+        else:
+            print("")

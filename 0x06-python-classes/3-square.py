50d7f0fadb7f43105c2e6a681e00b2c634bbe988
la victoire <lavictoire633@gmail.com>
2023-06-27 07:05:53 -0700

    a class that defines a square with a method to calculate the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This class defines a square with the size attribute
+
+    The size attribute is a private instance attribute
+    It has a default value of 0 if nothing has been provided
+"""
+
+
+class Square:
+    """Defines a class with an attribute size"""
+    def __init__(self, size=0):
+        """The initialization of the class"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+
+    def area(self):
+        """This method calculates the area of a square"""
+        return (self.__size * self.__size)

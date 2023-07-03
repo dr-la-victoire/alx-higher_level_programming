fec405c5c9be0c88623a40f8ccf0cbb925b2b5db
la victoire <lavictoire633@gmail.com>
2023-07-03 10:08:54 -0700

    i forgot to actually set the value in the setter code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,17 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """The initialization of the class"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        elif not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self._width = width
-            self._height = height
+        self._width = width
+        self._height = height
 
     @property
     def width(self):
@@ -36,6 +27,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self._width = value
 
     @height.setter
     def height(self, value):
@@ -44,3 +37,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self._height = value

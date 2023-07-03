953868716b3224ee7ac45206e6293d085f1005ee
la victoire <lavictoire633@gmail.com>
2023-07-03 14:05:10 -0700

    fixing styling errors

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
         """This function is the garbage collector: for deleting an instance"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the biggest rectangle based on the area"""

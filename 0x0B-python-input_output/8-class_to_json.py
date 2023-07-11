3e8559014acb5b38a165ef83d3d13197d605ddd1
la victoire <lavictoire633@gmail.com>
2023-07-11 07:16:47 -0700

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """This module contains a function that returns the dict JSON serialization"""
-import json
 
 
 def class_to_json(obj):
-    """Returns the dict repr of JSON serialization of an object"""
-    return json.dumps(obj)
+    """Returns the dict repr of JSON serialization of a data structure"""
+    return obj.__dict__

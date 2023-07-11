2906d7fde6f9518dabce405150f3be2a88331923
la victoire <lavictoire633@gmail.com>
2023-07-10 18:57:48 -0700

    solution to task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@
 
 def add_attribute(mc, name, attr):
     """This function sets the attribute of an object"""
-    if not hasattr(mc, "__dict__"): #  cos obj has no __dict__ attr
+    if not hasattr(mc, "__dict__"):  # cos obj has no __dict__ attr
         raise TypeError("can't add new attribute")
     else:
         setattr(mc, name, attr)

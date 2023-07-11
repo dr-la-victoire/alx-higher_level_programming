1f42639ded0750cc4266203c5211ddde2aaeed74
la victoire <lavictoire633@gmail.com>
2023-07-11 05:14:24 -0700

    fixing stylin g errors

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 def write_file(filename="", text=""):
     """Writes to a file and returns the number of chars written"""
     with open(filename, "w", encoding="utf-8") as the_file:
-       chars = the_file.write(text)
+        chars = the_file.write(text)
 
     return chars

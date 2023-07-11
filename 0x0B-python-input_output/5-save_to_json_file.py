e04d8ff63d1f3ffb6bf5cbec702382ad6d3b3226
la victoire <lavictoire633@gmail.com>
2023-07-11 06:41:05 -0700

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function that saves data to a file in JSON repr"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="utf-8") as the_json_file:
+        json.dump(my_obj, the_json_file)
+    the_json_file.close()

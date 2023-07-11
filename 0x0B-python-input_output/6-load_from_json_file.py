9a900d5633213de2f903146f07e45a153043dc21
la victoire <lavictoire633@gmail.com>
2023-07-11 06:51:47 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function that creates an obj from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file"""
+    with open(filename, "r", encoding="utf-8") as obj_file:
+        json.load(obj_file)

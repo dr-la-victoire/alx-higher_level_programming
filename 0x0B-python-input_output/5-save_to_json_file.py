39cca2b53b5f51eb49b20448478ea2b2d7135f0b
la victoire <lavictoire633@gmail.com>
2023-07-11 06:45:11 -0700

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Saves JSON data to a file"""
     with open(filename, "w", encoding="utf-8") as the_json_file:
         json.dump(my_obj, the_json_file)
     the_json_file.close()

d63b0684e84b5d556f3946ad082495fa3753bf20
la victoire <lavictoire633@gmail.com>
2023-07-10 19:05:13 -0700

    xxx

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module inherits from the int class"""
+
+
+class MyInt(int):
+    """showcasses the rebel integer"""
+
+    def MyInt(self):
+        """goes out of normal"""
+        if type(MyInt) == int:
+            return True
+        else:
+            return False

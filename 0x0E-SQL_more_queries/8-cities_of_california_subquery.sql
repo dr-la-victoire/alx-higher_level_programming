67b9174c6882b5a40837e12ca0289e62bfb9c922
la victoire <lavictoire633@gmail.com>
2023-08-16 12:28:37 -0700

    Task 8: Fixing errors

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,12 @@
--- This script lists all the cities in Carlifornia
--- which can be found in the USA database
+-- This script lists all the cities of California
+-- The cities are found in the USA database
 
-SELECT id, name 
-FROM cities 
+SELECT id, name
+FROM cities
 WHERE state_id = (
-	SELECT *
+	SELECT id
 	FROM states
-	WHERE name = 'California');
+	WHERE name = 'California'
+)
 ORDER BY id ASC;
 

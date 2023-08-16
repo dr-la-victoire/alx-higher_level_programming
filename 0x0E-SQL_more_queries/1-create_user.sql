7a9a31e2c619ae99f71ec53666132066d7bb76c9
la victoire <lavictoire633@gmail.com>
2023-08-16 06:59:47 -0700

    Task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates a user in the MySQL Server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+

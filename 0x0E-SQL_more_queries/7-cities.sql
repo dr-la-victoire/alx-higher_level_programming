12ef6ffb5f1aceb6f3a167a8b39a136c75f7bbef
la victoire <lavictoire633@gmail.com>
2023-08-16 11:59:50 -0700

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,11 +2,10 @@
 -- It has a table for the states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 	);
 

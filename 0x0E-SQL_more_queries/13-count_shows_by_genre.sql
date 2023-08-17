bc559913b0d51132ce688ab5117604c0d3ceaff7
la victoire <lavictoire633@gmail.com>
2023-08-17 13:23:10 -0700

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,8 +3,8 @@
 
 SELECT tv_genres.name AS 'genre', COUNT(tv_shows.title) AS 'number_of_shows'
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY 'genre'
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.name
 ORDER BY 'number_of_shows' DESC;
 

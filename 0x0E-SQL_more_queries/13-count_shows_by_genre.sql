4d14360c19d2a2c36ac91cdb5dea4193ce9b3559
la victoire <lavictoire633@gmail.com>
2023-08-18 01:47:17 -0700

    Correction on Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,10 @@
 -- This script counts the number of shows of each
 -- genre in the movie database
 
-SELECT tv_genres.name AS 'genre', COUNT(tv_shows.title) AS 'number_of_shows'
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_genres.name
-ORDER BY 'number_of_shows' DESC;
+ORDER BY number_of_shows DESC;
 

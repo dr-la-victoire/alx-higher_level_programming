a3a0a854fedf0e42f8e4a7f2bd34a79321d666fa
La Victoire <lavictoire633@gmail.com>
2023-09-11 07:36:28 -0700

    correction: removing the hasOwnProperty method

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let length = 0;
-let i;
-
-for (i in args) {
-  if (args.hasOwnProperty(i)) {
-    length++;
-  }
-}
-
-if (length < 3) {
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(args[2]);

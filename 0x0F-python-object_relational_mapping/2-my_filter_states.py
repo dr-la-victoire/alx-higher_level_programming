89d9baeeab6237117eeb4dc758d9be62153a3c66
La Victoire <lavictoire633@gmail.com>
2023-09-15 02:47:14 -0700

    fixing styling errors: under-indentation

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,12 +5,12 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-            user=argv[1], passwd=argv[2], db=argv[3])
+                         user=argv[1], passwd=argv[2], db=argv[3])
     match = argv[4]
     cur = db.cursor()
     cur.execute(
             "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                                                states.id".format(match))
+states.id".format(match))
     rows = cur.fetchall()
     for row in rows:
         print(row)

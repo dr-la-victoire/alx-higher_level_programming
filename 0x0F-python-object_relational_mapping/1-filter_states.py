a7abce1aac90fda913edd1416303e5a314477230
La Victoire <lavictoire633@gmail.com>
2023-09-15 01:24:06 -0700

    adding tasks 0-2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    This module selects all states
+    with letter N
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    queries = """
+                SELECT * FROM states
+                WHERE name LIKE 'N%'
+            """
+    cur.execute(queries)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

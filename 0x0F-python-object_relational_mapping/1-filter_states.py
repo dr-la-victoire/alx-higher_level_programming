dfb80376ec638b063774714a14fdc1d38247f2d3
La Victoire <lavictoire633@gmail.com>
2023-09-15 01:34:43 -0700

    a script that selects all the states starting with N from the dataabase

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,11 +11,7 @@ if __name__ == "__main__":
             host="localhost", port=3306,
             user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    queries = """
-                SELECT * FROM states
-                WHERE name LIKE 'N%'
-            """
-    cur.execute(queries)
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)

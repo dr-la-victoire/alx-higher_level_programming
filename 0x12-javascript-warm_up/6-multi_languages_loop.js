3640462b3dfd6a05470c269f037d4dfa5a9fbcb2
La Victoire <lavictoire633@gmail.com>
2023-09-21 12:24:11 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const arrLang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < arrLang.length; i++) {
   console.log(arrLang[i]);
 }

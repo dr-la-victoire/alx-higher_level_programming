2a2cfa23be22eaed3cc8804fbec5ce568e56a20d
La Victoire <lavictoire633@gmail.com>
2023-09-21 12:09:42 -0700

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+const a = "C is fun";
+const b = "Python is cool";
+const c = "JavaScript is amazing";
+
+console.log(a);
+console.log(b);
+console.log(c);

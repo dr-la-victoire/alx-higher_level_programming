6643ddb801e13ad1f055140bcbe2aecb6e9cab34
La Victoire <lavictoire633@gmail.com>
2023-09-27 12:28:09 -0700

    Task 3: adding a method to the class

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,8 +6,9 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
-    for (let i = 0; i < this.height; i++) 
+    for (let i = 0; i < this.height; i++) {
       let x = '';
       for (let j = 0; j < this.width; j++) {
         x += 'X';
@@ -16,5 +17,4 @@ class Rectangle {
     }
   }
 }
-
 module.exports = Rectangle;

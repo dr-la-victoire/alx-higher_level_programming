8f785b67f9eb5b47f5b26569a1c10a766e41c1fc
La Victoire <lavictoire633@gmail.com>
2023-09-27 14:00:10 -0700

    Task 6: Correction on importing the previous square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const task5Square = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends task5Square {
   constructor (size) {
     super(size, size);
   }

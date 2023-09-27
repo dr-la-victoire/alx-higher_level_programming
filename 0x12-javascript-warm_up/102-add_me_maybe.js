c7cde1ba060c13699ce32ad1bd684b9b9e1ded27
La Victoire <lavictoire633@gmail.com>
2023-09-27 05:05:53 -0700

    Task 102: Incrementing and calling a function

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number++); // increment and call
+  theFunction(++number); // increment and call
 };
 
 // exports.addMeMaybe exports the function to another file

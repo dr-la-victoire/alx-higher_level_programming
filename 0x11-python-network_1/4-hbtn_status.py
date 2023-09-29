9e2bd539c565f94eea4a4ea534d72d3a1ef0a2a9
La Victoire <lavictoire633@gmail.com>
2023-09-29 07:58:36 -0700

    Task 4: Fetching from a website with requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script fetches from a website"""
+import requests
+
+if __name__ == "__main__":
+    info = requests.get("https://alx-intranet.hbtn.io/status")
+    str_info = info.text
+    print(
+            "Body response:\n\t- type: {}\n\t- content: {}".format(
+                str_info, str_info))

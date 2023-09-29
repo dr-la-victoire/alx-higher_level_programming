e3d83063461782790fe8e84f0dd5233a46df3627
La Victoire <lavictoire633@gmail.com>
2023-09-29 02:04:27 -0700

    Task 0: Fetching a URL with urllib.request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,3 +4,7 @@ import urllib.request
 
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     html = response.read()
+print(
+        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
+                content: {}".format(
+                    html.__class__, html, html.decode("utf-8")))

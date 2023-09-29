b02667fcec7847c022ba97963fe09bf78db7ed16
La Victoire <lavictoire633@gmail.com>
2023-09-29 05:42:16 -0700

    same issue: forgot to add the whole if __name__ == __main__ clasue

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,11 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    html = response.headers
-    header_dict = dict(html)
-header_variable = header_dict.get("X-Request-Id")
-print(header_variable)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.headers
+        header_dict = dict(html)
+    for header, value in header_dict.items():
+        if header == "X-Request-Id":
+            print("{}".format(value))

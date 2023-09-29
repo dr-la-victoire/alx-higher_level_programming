efe52d14987b4a2e70131adf204841245d1eac85
La Victoire <lavictoire633@gmail.com>
2023-09-29 05:46:43 -0700

    Task 0: correcting the mistake like the others

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,11 @@
 """This module fetches a particular website"""
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
-print(
-        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 "
-        "content: {}".format(
-                    html.__class__, html, html.decode("utf-8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+    print(
+            "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 "
+            "content: {}".format(
+                        html.__class__, html, html.decode("utf-8")))

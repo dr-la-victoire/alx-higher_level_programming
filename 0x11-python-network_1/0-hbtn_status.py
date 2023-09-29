bbf879b2f3c38c3719e32736959457d6075d9e13
La Victoire <lavictoire633@gmail.com>
2023-09-29 04:26:11 -0700

    Task 0: Styling corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     html = response.read()
 print(
-        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 \
-                content: {}".format(
+        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 "
+        "content: {}".format(
                     html.__class__, html, html.decode("utf-8")))

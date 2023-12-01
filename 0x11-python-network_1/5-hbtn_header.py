bf134d896b4f59b9d28e49103f65af28edfd5e39
dr-la-victoire <126200439+dr-la-victoire@users.noreply.github.com>
2023-12-01 19:13:27 +0100

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This script gets the value of a header variable"""
-import requests
-import sys
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    info = requests.get(url)
-    print("{}".format(info.headers.get("X-Request-Id")))
--- 0x11-python-network_1/6-post_email.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module sends a POST request"""
-import sys
-import requests
-
-if __name__ == "__main__":
-    info = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-    print("{}".format(info.text))

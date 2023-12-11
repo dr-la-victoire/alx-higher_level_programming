6dccd81698416d91e07830410313efaa79193ed4
La Victoire <lavictoire633@gmail.com>
2023-12-11 08:26:45 -0800

    Task 5: program to check if an argument is a number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let num = process.argv[2];
+const newNum = parseInt(num); 
+
+if (isNaN(newNum)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + newNum);
+}

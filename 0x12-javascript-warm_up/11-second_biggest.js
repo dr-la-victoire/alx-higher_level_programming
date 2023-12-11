aecfb381206518eadadb42d867e000df9715a45d
La Victoire <lavictoire633@gmail.com>
2023-12-11 13:29:55 -0800

    I know I am pushing incorrrect code

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const len = process.argv.length;
+
+// printing 1 if args are 0 or 1
+if (len <= 3) {
+  console.log(1);
+} else {
+  // assigning the first and second args as biggest and 2nd biggest
+  let biggest = parseInt(process.argv[2]);
+  let secondBiggest = parseInt(process.argv[3]);
+  
+  // swaping if second biggest is actually bigger than the biggest
+  if (secondBiggest > biggest) {
+    let a = secondBiggest;
+    secondBiggest = biggest;
+    biggest = a;
+  }
+
+  // going through the rest of the array
+  for (let i = 4; i < len; i++) {
+    // turning i to a number
+    let num = parseInt(process.argv[i]);
+
+    // checking if the current is the biggest
+    if (num > biggest) {
+      biggest = num;
+    } else if (num > secondBiggest) {
+      secondBiggest = num;
+    }
+  }
+  console.log(secondBiggest);
+}

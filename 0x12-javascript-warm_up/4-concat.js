98b0aa36e4da4f579490ca784010f8adaccc9d37
La Victoire <lavictoire633@gmail.com>
2023-12-11 04:50:03 -0800

    Task 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+if (process.argv[2] !== null && process.argv[3] !== null) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else {
+  console.log('x' + ' is ' + 'x');
+}

5f976dce95d6036843dc195c4b9d7b430a825e0f
La Victoire <lavictoire633@gmail.com>
2023-12-11 09:05:09 -0800

    Task 9: First JavaScript function... Also fixed semistandard errors

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   a = parseInt(process.argv[2]);
   b = parseInt(process.argv[3]);
-  
+
   if (isNaN(a) || isNaN(b)) {
     console.log(NaN);
   } else {

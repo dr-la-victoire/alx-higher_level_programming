a9b308050a076b8fa137e51ca9d43b1928c1d270
La Victoire <lavictoire633@gmail.com>
2023-12-13 02:59:48 -0800

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const len = list.length;
+  const newList = [];
+
+  for (let i = len - 1; i >= 0; i--) {
+    newList.push(list[i]);
+  }
+
+  return newList;
+};

42ea06d00d6c936b54db473c921734172f1fa82b
La Victoire <lavictoire633@gmail.com>
2023-12-14 07:09:41 -0800

    Tasks 0 - 2: Getting started with the MySQLdb library

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module lists all the states from a special database"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)

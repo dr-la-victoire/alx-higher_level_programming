a424bdc64f91e5dbf6a94726bb1c5f7624fb59fb
La Victoire <lavictoire633@gmail.com>
2023-12-15 05:21:00 -0800

    Task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This model contains the State class for the ORM"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This is the State Class"""
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)

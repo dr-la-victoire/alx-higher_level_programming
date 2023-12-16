ff6fc60d2df5e3c3b2f62db05209b122115e3f68
La Victoire <lavictoire633@gmail.com>
2023-12-16 00:23:14 -0800

    Task 14: Establishing the foreign key relationship.... fixing pycodestyle errors

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     cities = session.query(City, State).join(State).all()
     for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-

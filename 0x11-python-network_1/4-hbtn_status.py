1bfe9cbd810d1b096120a32a99cc67e83575ecd0
La Victoire <lavictoire633@gmail.com>
2024-01-07 07:25:35 -0800

    Task 4: Using requests to get a webpage

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This script fetches a URL with requests"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
